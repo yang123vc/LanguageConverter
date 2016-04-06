984429c7189b602127f75205f6e2ecb6f33fa9a4
Vladimir Rudnev <artmanyak@gmail.com>
2016-04-07 02:46:29 +0500

    Ночной бред

--- LanguageConverter/LanguageTranslator/ModuleTranslator.cs
+++ LanguageConverter/LanguageTranslator/ModuleTranslator.cs
@@ -1,8 +1,11 @@
-﻿using LanguageTranslator.CodeGen.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using LanguageTranslator.CodeGen.Interfaces;
 using LanguageTranslator.ExtensionPoints;
 using LanguageTranslator.Java;
 using LanguageTranslator.Java.Interfaces;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LanguageTranslator
 {
@@ -29,6 +32,15 @@ namespace LanguageTranslator
                     Declarations = new IDeclarationNode[0]
                 };
             }
+            var classTranslator = new ClassTranslator();
+            var descedantNodes = rootNode.DescendantNodes();
+            var declarations = new List<IDeclarationNode>();
+            declarations.AddRange(descedantNodes.OfType<ClassDeclarationSyntax>().Select(decl => classTranslator.Translate(decl)));
+
+            return new JavaSyntaxTree
+            {
+                Declarations = declarations.ToArray()
+            };
         }
     }
 }
