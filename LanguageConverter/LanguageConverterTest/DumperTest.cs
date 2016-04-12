4cddecead095cfa64d15dea2117d0fa6bf31bad9
Vladimir Rudnev <artmanyak@gmail.com>
2016-04-12 13:49:01 +0500

    небольшой тестик, чтобы смотреть AST

---
+++ LanguageConverter/LanguageConverterTest/DumperTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using LanguageTranslator;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LanguageConverterTest
+{
+    [TestClass]
+    public class DumperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const string csCode = "class A { void foo() { var x = new int[] { 1,2,3};}  }";
+            var syntaxTree = SyntaxFactory.ParseSyntaxTree(csCode);
+            var builder = new StringBuilder();
+            using (var writer = new StringWriter(builder))
+            {
+                var dumper = new ASTDumper(syntaxTree.GetRoot(), writer);
+                dumper.Dump();
+            }
+            Console.Write(builder.ToString());
+        }
+    }
+}
---
+++ LanguageConverter/LanguageTranslator/ASTDumper.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis;
+
+namespace LanguageTranslator
+{
+    public class ASTDumper
+    {
+        private readonly SyntaxNode rootNode;
+        private readonly TextWriter outputWriter;
+
+        public ASTDumper(SyntaxNode rootNode, TextWriter outputWriter)
+        {
+            this.rootNode = rootNode;
+            this.outputWriter = outputWriter;
+        }
+
+        public void Dump()
+        {
+            Dump(rootNode, 0);
+        }
+
+        private void Dump(SyntaxNode node, int deep)
+        {
+            var spaces = new string(' ', deep * 2);
+            outputWriter.Write(spaces);
+            outputWriter.WriteLine(node.GetType().Name);
+            foreach (var childNode in node.ChildNodes())
+            {
+                Dump(childNode, deep + 1);
+            }
+        }
+    }
+}
